012e3915f6d1fe3aa0bba2fad5f03a5e545446ed
Estefany palomino <eangelicapalomino@gmail.com>
2025-11-18 22:55:08 -0500

    ver

---
+++ mostrar_errores.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Probando carga de procesar_login<br>";
+
+require __DIR__ . '/acceso/procesar_login.php';
