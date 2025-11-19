13997b96ac0299fc9985103ee8ea20d377171c0b
Estefany palomino <eangelicapalomino@gmail.com>
2025-11-19 16:06:27 -0500

    nav

--- navbar.php
+++ navbar.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
     <div class="container">
-        <a class="navbar-brand" href="#">Sistema de Parqueadero</a>
+        <a class="navbar-brand" href="<?php echo BASE_URL; ?>">Sistema de Parqueadero</a>
         <div class="navbar-nav">
             <?php if (isset($_SESSION['usuario_id'])): ?>
-                <a class="nav-link" href="../paneles/administrador.php">Panel</a>
-                <a class="nav-link" href="../acceso/logout.php">Cerrar Sesión</a>
+                <a class="nav-link" href="<?php echo BASE_URL; ?>/paneles/administrador.php">Panel</a>
+                <a class="nav-link" href="<?php echo BASE_URL; ?>/acceso/logout.php">Cerrar Sesión</a>
             <?php endif; ?>
         </div>
     </div>
